a0b0193339fc6ddf8faf94a78a7182c2fbe015d4
Zagaz <andre_ranulfo@hotmail.com>
2024-08-31 17:49:06 -0300

    chore: Refactor form in index.html to handle different types of customers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-console.log("OK")
+function tipoDePessoa() {
+  const pessoaFisica = document.getElementById("pessoaFisica");
+  const pessoaJuridica = document.getElementById("pessoaJuridica");
+  const cpfCnpjId = document.getElementById("cpfCnpjId");
+  const cpfCnpj = document.getElementById("cpfCnpj");
+
+  pessoaFisica.addEventListener("change", () => {
+    cpfCnpj.placeholder = "CPF";
+    cpfCnpjId.innerHTML = "CPF";
+  });
+
+  pessoaJuridica.addEventListener("change", () => {
+    cpfCnpj.placeholder = "CNPJ";
+    cpfCnpjId.innerHTML = "CNPJ";
+  });
+}
